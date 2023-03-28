35b3d5c9e84813d73ced189da41c73a523efb447
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-28 13:02:50 -0300

    modularized css

---
+++ fundamentos1/src/pages/estiloso.jsx
@@ -0,0 +1,8 @@
+import styles from './Estiloso.module.css'
+export default function Estiloso(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estilo usando css modulos</h1>
+        </div>
+    )
+}
