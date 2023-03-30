eac7a4f4b5dc533ce9f5fac54f0f6159dc636202
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-30 12:58:42 -0300

    tables

--- next-crud/pages/index.tsx
+++ next-crud/pages/index.tsx
@@ -1,6 +1,18 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const clientes=[
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 28, '2'),
+    new Cliente('Pedro', 19, '3'),
+    new Cliente('Joao', 83, '4')
+  ]
+  function clienteSelecionado=(cliente:Cliente){
+    
+  }
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -8,7 +20,8 @@ export default function Home() {
       text-white
     `}>
    <Layout titulo="Cadastro Simples">
-    <span>Conteudo </span>
+    <Tabela clientes={clientes} 
+    clienteSelecionado={clienteSelecionado}></Tabela>
    </Layout>
     </div>
   )
